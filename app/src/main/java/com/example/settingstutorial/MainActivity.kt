26d162e124e7e69e8535d44494f6ed8b73e36064
aguestuser <aguestuser@riseup.net>
2022-10-23 17:20:42 -0400

    [03] add project baseline

---
+++ app/src/main/java/com/example/settingstutorial/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.settingstutorial
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.example.settingstutorial.composables.Settings
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent { Settings() }
+  }
+}
