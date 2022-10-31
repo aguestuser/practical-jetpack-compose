ac965e05ab2170839f6a4044e817313016444d1b
aguestuser <aguestuser@riseup.net>
2022-10-31 13:14:00 -0400

    [03] show how to use `update` instead of setting value on flows!

--- 03_settings/app/src/main/java/com/example/settingstutorial/SettingsViewModel.kt
+++ 03_settings/app/src/main/java/com/example/settingstutorial/SettingsViewModel.kt
@@ -3,12 +3,14 @@ package com.example.settingstutorial
 import androidx.lifecycle.ViewModel
 import com.example.settingstutorial.ui.theme.Theme
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
 
 class SettingsViewModel : ViewModel() {
   val uiState = MutableStateFlow(SettingsState())
 
   fun toggleNotificationsSetting(newSetting: Boolean) {
     uiState.value = uiState.value.copy(notificationsEnabled = newSetting)
+    uiState.update { it.copy(notificationsEnabled = newSetting) }
   }
 
   fun toggleHintsSetting(newSetting: Boolean) {
